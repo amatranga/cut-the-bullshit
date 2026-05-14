934dc40de9b8988ad43d086ba45c6d68302890ca
Alex Matranga <24361338+amatranga@users.noreply.github.com>
2026-05-14 13:52:46 -0500

    [feat]: Show better error messages to user

--- app/components/ErrorMessage.tsx
+++ app/components/ErrorMessage.tsx
@@ -3,8 +3,14 @@ type ErrorMessageProps = {
 }
 
 const ErrorMessage = ({ message }: ErrorMessageProps) => (
-  <div className="rounded-xl border border-red-500/30 bg-red-500/10 p-4 text-red-200">
-    {message}
+  <div className="rounded-xl border border-red-500/30 bg-red-500/10 p-4 text-red-100">
+    <p className="text-xs uppercase tracking-[0.25em] text-red-300">
+      Strategic Escalation Notice
+    </p>
+
+    <p className="mt-2">
+      {message}
+    </p>
   </div>
 );
 
